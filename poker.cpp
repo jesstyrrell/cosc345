97b4f3c22761ccaf847c37f1e663df17a471faad
tyrje837 <tyrje837@student.otago.ac.nz>
2024-07-22 12:41:16 +1200

    Refactor Player class to use pointers for hand and add methods for managing hand

--- poker.cpp
+++ poker.cpp
@@ -6,8 +6,13 @@
 #include <random>
 #include <ctime>
 
+// Card class
+// Represents a single card in a deck of cards
+// Contains a suit and a rank
+// Example: Card card = Card("Hearts", "Ace");
 
 class Card {
+    
     private:
         std::string suit;
         std::string rank;
@@ -54,7 +59,6 @@ class Deck {
             return card;
         }
 
-
 };
 
 class Player {
@@ -71,44 +75,59 @@ class Player {
         }
         
         void bet(int amount) {
-            stack -= amount;
+            this->stack -= amount;
         }
 
         void win(int amount) {
-            stack += amount;
+            this->stack += amount;
         }
 
         int get_stack() {
-            return stack;
+            return this->stack;
         }
 
         std::string get_name() {
-            return name;
+            return this->name;
         }
 
         std::vector<Card> get_hand() {
-            return hand;
+            return this->hand;
+        }
+
+        void add_card_to_hand(Card card) {
+            this->hand.push_back(card);
+        }
+
+        void clear_hand() {
+            this->hand.clear();
+        }
+
+        void show_hand() {
+            for (Card card : hand) {
+                std::cout << card.get_rank() << " of " << card.get_suit() << std::endl;
+            }
         }
 };
 
 class Game{
     private:
         Deck deck;
-        std::vector<Player> players;
+        std::vector<Player*> players;
         std::vector<Card> community_cards;
         int pot = 0;
 
     public:
-        Game(std::vector<Player> players) {
+        Game(std::vector<Player*>& players) {
             this->players = players;
             this->deck = Deck();
             this->deck.shuffle();
         }
 
         void deal_hands() {
-            for (Player player : players) {
-                player.get_hand().push_back(deck.deal());
-                player.get_hand().push_back(deck.deal());
+            for (Player* player : players) {
+                player->add_card_to_hand(deck.deal()); // Deal the first card to the player
+                player->add_card_to_hand(deck.deal()); // Deal the second card to the player
+                
             }
         }
 
@@ -136,11 +155,62 @@ class Game{
         }
 
         void show_player_hands() {
-            for (Player player : players) {
-                std::cout << player.get_name() << "'s hand: " << std::endl;
-                for (Card card : player.get_hand()) {
+            for (Player* player : players) {
+                std::cout << player->get_name() << "'s hand: " << std::endl;
+                for (Card card : player->get_hand()) {
                     std::cout << card.get_rank() << " of " << card.get_suit() << std::endl;
                 }
+                std::cout << "--------------" << std::endl;
             }
         }
+
+        std::vector<Player*> get_players() {
+            return players;
+        }
 };
+
+// A main method for testing the classes
+int main() {
+    Player player1 = Player("Jess", 1000);
+    Player player2 = Player("James", 1000);
+    Player player3 = Player("Corban", 1000);
+    // Create a vector of pointers 
+    std::vector<Player*> playerPointers;
+
+    playerPointers.push_back(&player1);
+    playerPointers.push_back(&player2);
+    playerPointers.push_back(&player3);
+
+    Game game = Game(playerPointers); // i swear you have to pass a pointer, but also its a list of two players
+    game.deal_hands();
+    game.show_player_hands();
+    game.deal_flop();
+    game.deal_turn();
+    game.deal_river();
+    game.show_community_cards();
+    std::cout << "--------------" << std::endl;
+    // Print the stack of each player 
+    for (Player* player : game.get_players()) {
+        std::cout << player->get_name() << "'s stack: " << player->get_stack() << std::endl;
+        std::cout << "    -----    " << std::endl;
+    }
+    player1.bet(100);
+    player2.bet(200);
+    player3.bet(300);
+    for (Player* player : game.get_players()) {
+        std::cout << player->get_name() << "'s stack: " << player->get_stack() << std::endl;
+        std::cout << "    -----    " << std::endl;
+    }
+
+    player1.win(1000);
+    player2.win(2000);
+    player3.win(3000);
+
+    for (Player* player : game.get_players()) {
+        std::cout << player->get_name() << "'s stack: " << player->get_stack() << std::endl;
+        std::cout << "    -----    " << std::endl;
+    }
+
+
+    return 0;
+}
