875f5a5eee521eb7e707420b82651cb3f151968b
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-08-16 15:24:44 +1200

    feat: Add GUI class for user interaction and display

--- src/Game.hpp
+++ src/Game.hpp
@@ -2,30 +2,42 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
 
 #include "Player.hpp"
 #include "Deck.hpp"
 #include "Card.hpp"
 
-using namespace std; 
+enum Move { CHECK, CALL, RAISE, FOLD };
 
 class Game {
     private:
         Deck deck;
         vector<Player*> players;
         vector<Card> community_cards;
+
+        const int BIG_BLIND = 10;
+        const int SMALL_BLIND = 5;
         int pot = 0;
+        int button = 0; 
+        
+        Move getCurrentMove(std::string move);
+
+        void addBlindsToPot(Player *bigBlindPlayer, Player *smallBlindPlayer);
 
     public:
         Game(vector<Player*>& players);
         void add_player(Player *player);
+
         void deal_hands();
         void deal_flop();
         void deal_turn();
         void deal_river();
-        void show_community_cards();
-        void show_player_hands();
+
         vector<Player*> get_players();
+
+        void playHand(); 
+        void makeMoveForUser(std::string move, Player *player);
+
+
         bool static test_game();
 };
