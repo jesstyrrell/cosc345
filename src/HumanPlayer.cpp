cf79b915a3a908c3716fe48d0064681c03b1a242
tyrje837 <tyrje837@student.otago.ac.nz>
2024-08-22 11:51:24 +1200

    Extend player for a human and random player

---
+++ src/HumanPlayer.cpp
@@ -0,0 +1,18 @@
+#include "Player.hpp"
+#include "GUI.hpp"
+
+class HumanPlayer : public Player {
+public:
+    HumanPlayer(const std::string& name, int stack) : Player(name, stack) {}
+
+    std::string getMove(bool canCheck, bool canRaise, bool canFold, bool canCall) override {
+        // Call GUI to get user move
+        return GUI::getUserMove(canCheck, canRaise, canFold, canCall);
+    }
+
+    int getBetSizing(int minBet, int maxBet) override {
+        // Call GUI to get bet sizing
+        return GUI::getBetSizing(minBet, maxBet);
+    }
+
+};
---
+++ src/RandomPlayer.cpp
@@ -0,0 +1,48 @@
+#include "Player.hpp"
+#include "GUI.hpp"
+#include <random>
+#include <chrono>
+#include <algorithm>
+
+class RandomPlayer : public Player {
+private:
+    std::default_random_engine rng;
+
+public:
+    RandomPlayer(const std::string& name, int stack) : Player(name, stack) {
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        rng.seed(seed);
+    }
+
+    std::string getMove(bool canCheck, bool canRaise, bool canFold, bool canCall) override {
+        vector<std::string> moves = {"a", "r", "f", "c"};
+        // randomly select one of the bools that is true 
+        std::vector<bool> possibleMoves = {canCheck, canRaise, canFold, canCall};
+        std::uniform_int_distribution<int> dist(0, 3);
+        int moveIndex = dist(rng);
+        while (!possibleMoves[moveIndex]) {
+            moveIndex = dist(rng);
+        }
+        // TESTING: print the move 
+        std::cout << moves[moveIndex] << std::endl;
+        return moves[moveIndex];
+    }
+
+    int getBetSizing(int minBet, int maxBet) override {
+        // randomly flip a coin until it is false and count the number of flips
+        int count = 0;
+
+        while(true){
+            srand (time(NULL));
+            int flip = (rand()%2)+ 1 ;
+            if (flip == 1){
+               break;
+            }
+            count++;
+        }
+        // TESTING: print the bet sizing
+        std::cout << min(minBet*count, maxBet) << std::endl;
+        return min(minBet*count, maxBet);
+    }
+    
+};
