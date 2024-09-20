89692aaa12e44595a50ba3473b645141bc734f7c
James Robiony-Rogers <43721451+JamesRobionyRogers@users.noreply.github.com>
2024-09-20 12:00:59 +1200

    Improve code coverage across the application (#37)
    * Remove FIXME comment
    
    * Refactor test_Deck.cc to add new test cases for finding and removing cards
    
    * tests: Increase code coverage
    
    * tests: Remove test_game method in Game class
    
    * tests: Improve coverage Game::deal() method
    
    * tests: Write passing test for EquityCalculator::calculateHandEquity()

--- tests/test_GUI.cc
+++ tests/test_GUI.cc
@@ -1,175 +1,157 @@
 // #include <gtest/gtest.h>
-// #include <vector>
-// #include <string>
 // #include <sstream>
-// #include <iostream>
+// #include <fstream>
 
 // #include "../src/GUI.hpp"
 // #include "../src/Game.hpp"
 // #include "../src/Player.hpp"
-// #include "../src/Card.hpp"
 
-// class GUITest : public ::testing::Test
-// {
+// class Player; // Forward declaration
+
+// class GUITest : public ::testing::Test {
 // protected:
-//     GUI *gui;
-//     Game *game;
-//     Player *player1;
-//     Player *player2;
+//     GUI gui;
+//     Game game;
 
-//     void SetUp() override
-//     {
-//         gui = new GUI();
-
-//         player1 = std::make_unique<Player>("Player1", 1000);
-//         player2 = std::make_unique<Player>("Player2", 1000);
-
-//         player1 = new Player("Player1", 1000);
-//         player2 = new Player("Player2", 1000);
-
-//         vector<Player *> players = {player1, player2};
-//         game = new Game(players);
-
-//         GUI::setGame(game);
+//     void SetUp() override {
+//         GUI::setGame(&game);
 //     }
 
-//     void TearDown() override
-//     {
-//         delete gui;
-//         delete game;
-//         delete player1;
-//         delete player2;
+//     // Helper function to capture cout output
+//     std::string captureOutput(std::function<void()> func) {
+//         std::stringstream buffer;
+//         std::streambuf* old = std::cout.rdbuf(buffer.rdbuf());
+//         func();
+//         std::cout.rdbuf(old);
+//         return buffer.str();
 //     }
 // };
 
-// TEST_F(GUITest, GetUserMove)
-// {
+// TEST_F(GUITest, SetAndGetGame) {
+//     Game* testGame = new Game();
+//     GUI::setGame(testGame);
+//     EXPECT_EQ(&GUI::getGame(), testGame);
+//     delete testGame;
+// }
+
+// TEST_F(GUITest, ClearScreen) {
+//     std::string output = captureOutput([]() { GUI::clearScreen(); });
+//     #ifdef _WIN32
+//     EXPECT_EQ(output, "cls");
+//     #else
+//     EXPECT_EQ(output, "clear");
+//     #endif
+// }
+
+// TEST_F(GUITest, GetUserMove) {
 //     std::istringstream input("f\n");
 //     std::cin.rdbuf(input.rdbuf());
-
-//     std::string move = gui->getUserMove(true, true, true, true);
-//     EXPECT_EQ(move, "f");
+//     EXPECT_EQ(GUI::getUserMove(true, true, true, true), "f");
 // }
 
-// TEST_F(GUITest, GetBetSizing)
-// {
+// TEST_F(GUITest, GetBetSizing) {
 //     std::istringstream input("50\n");
 //     std::cin.rdbuf(input.rdbuf());
-
-//     int bet = gui->getBetSizing(10, 100);
-//     EXPECT_EQ(bet, 50);
+//     EXPECT_EQ(GUI::getBetSizing(10, 100), 50);
 // }
 
-// TEST_F(GUITest, DisplayCommunityCards)
-// {
-//     std::vector<Card> communityCards = {Card("Hearts", "2"), Card("Diamonds", "3")};
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayCommunityCards(communityCards);
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = "2 of Hearts\n3 of Diamonds\n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayCommunityCards) {
+//     std::vector<Card> communityCards = {Card("Hearts", "A"), Card("Spades", "K")};
+//     std::string output = captureOutput([&]() { GUI::displayCommunityCards(communityCards); });
+//     EXPECT_TRUE(output.find("A of Hearts") != std::string::npos);
+//     EXPECT_TRUE(output.find("K of Spades") != std::string::npos);
 // }
 
-// TEST_F(GUITest, DisplayAllPlayerHands)
-// {
-//     player1->addCardToHand(Card("Hearts", "2"));
-//     player2->addCardToHand(Card("Diamonds", "3"));
-//     std::vector<Player *> players = {player1, player2};
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayAllPlayerHands(players);
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = "Player1's hand: \n2 of Hearts\n--------------\nPlayer2's hand: \n3 of Diamonds\n--------------\n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayAllPlayerHands) {
+//     std::vector<Player*> players = {new Player("Alice", 1000), new Player("Bob", 1000)};
+//     players[0]->add_card(Card("Hearts", "A"));
+//     players[0]->add_card(Card("Spades", "K"));
+//     players[1]->add_card(Card("Diamonds", "Q"));
+//     players[1]->add_card(Card("Clubs", "J"));
+//     std::string output = captureOutput([&]() { GUI::displayAllPlayerHands(players); });
+//     EXPECT_TRUE(output.find("Alice's hand:") != std::string::npos);
+//     EXPECT_TRUE(output.find("Bob's hand:") != std::string::npos);
+//     EXPECT_TRUE(output.find("A of Hearts") != std::string::npos);
+//     EXPECT_TRUE(output.find("J of Clubs") != std::string::npos);
+//     for (auto player : players) delete player;
 // }
 
-// TEST_F(GUITest, DisplayPlayerHand)
-// {
-//     player1->addCardToHand(Card("Hearts", "2"));
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayPlayerHand(player1);
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = "Player1's hand: \n2 of Hearts\n--------------\n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayPlayerHand) {
+//     Player* player = new Player("Alice", 1000);
+//     player->add_card(Card("Hearts", "A"));
+//     player->add_card(Card("Spades", "K"));
+//     std::string output = captureOutput([&]() { GUI::displayPlayerHand(player); });
+//     EXPECT_TRUE(output.find("Alice's hand:") != std::string::npos);
+//     EXPECT_TRUE(output.find("A of Hearts") != std::string::npos);
+//     EXPECT_TRUE(output.find("K of Spades") != std::string::npos);
+//     delete player;
 // }
 
-// TEST_F(GUITest, DisplayPlayerStack)
-// {
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayPlayerStack(player1);
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = "Player1's stack: 1000\n    -----    \n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayStartScreen) {
+//     std::string output = captureOutput([]() { GUI::displayStartScreen(); });
+//     EXPECT_TRUE(output.find("POKER CLI") != std::string::npos);
 // }
 
-// TEST_F(GUITest, DisplayStartScreen)
-// {
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayStartScreen();
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = R"(:::::::::  :::    ::: ::::    :::    ::::::::::: :::::::::::    ::::::::::: :::       ::: :::::::::::  ::::::::  :::::::::: 
-// :+:    :+: :+:    :+: :+:+:   :+:        :+:         :+:            :+:     :+:       :+:     :+:     :+:    :+: :+:        
-// +:+    +:+ +:+    +:+ :+:+:+  +:+        +:+         +:+            +:+     +:+       +:+     +:+     +:+        +:+        
-// +#++:++#:  +#+    +:+ +#+ +:+ +#+        +#+         +#+            +#+     +#+  +:+  +#+     +#+     +#+        +#++:++#   
-// +#+    +#+ +#+    +#+ +#+  +#+#+#        +#+         +#+            +#+     +#+ +#+#+ +#+     +#+     +#+        +#+        
-// #+#    #+# #+#    #+# #+#   #+#+#        #+#         #+#            #+#      #+#+# #+#+#      #+#     #+#    #+# #+#        
-// ###    ###  ########  ###    ####    ###########     ###            ###       ###   ###   ###########  ########  ########## )";
-//     expectedOutput += "\n--------------------------------\n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayEndMessage) {
+//     std::string output = captureOutput([]() { GUI::displayEndMessage(); });
+//     EXPECT_TRUE(output.find("cardAnimation") != std::string::npos);
 // }
 
-// TEST_F(GUITest, DisplayEndMessage)
-// {
-//     // This test is more complex due to the file reading and animation.
-//     // You might need to mock file reading and time delays for a proper unit test.
-// }
-
-// TEST_F(GUITest, DisplayMenu)
-// {
+// TEST_F(GUITest, DisplayMenu) {
 //     std::istringstream input("\n");
 //     std::cin.rdbuf(input.rdbuf());
-
-//     int result = gui->displayMenu();
-//     EXPECT_EQ(result, 1);
+//     EXPECT_EQ(GUI::displayMenu(), 1);
 // }
 
-// TEST_F(GUITest, EndOfRoundMenu)
-// {
+// TEST_F(GUITest, SignInMenu) {
+//     std::istringstream input("3\nTestPlayer\n");
+//     std::cin.rdbuf(input.rdbuf());
+//     EXPECT_EQ(GUI::signInMenu(), "TestPlayer");
+// }
+
+// TEST_F(GUITest, GetNumberOfPlayers) {
+//     std::istringstream input("4\n");
+//     std::cin.rdbuf(input.rdbuf());
+//     EXPECT_EQ(GUI::getNumberOfPlayers(), 4);
+// }
+
+// TEST_F(GUITest, EndOfRoundMenu) {
 //     std::istringstream input("\n");
 //     std::cin.rdbuf(input.rdbuf());
-
-//     int result = gui->endOfRoundMenu();
-//     EXPECT_EQ(result, 1);
+//     EXPECT_EQ(GUI::endOfRoundMenu(), 1);
 // }
 
-// TEST_F(GUITest, DisplayPlayerMove)
-// {
-//     std::ostringstream output;
-//     std::streambuf *oldCoutBuf = std::cout.rdbuf(output.rdbuf());
-
-//     gui->displayPlayerMove(player1, "raise", 50);
-
-//     std::cout.rdbuf(oldCoutBuf);
-//     std::string expectedOutput = "Player1: raise 50\n";
-//     EXPECT_EQ(output.str(), expectedOutput);
+// TEST_F(GUITest, DisplayPlayerStack) {
+//     Player* player = new Player("Alice", 1000);
+//     std::string output = captureOutput([&]() { GUI::displayPlayerStack(player); });
+//     EXPECT_TRUE(output.find("Alice's stack: 1000") != std::string::npos);
+//     delete player;
 // }
 
-// int main(int argc, char **argv)
-// {
-//     ::testing::InitGoogleTest(&argc, argv);
+// TEST_F(GUITest, DisplayGameState) {
+//     // Set up a game state
+//     game.addPlayer(new Player("Alice", 1000));
+//     game.addPlayer(new Player("Bob", 1000));
+//     game.getCommunityCards().push_back(Card("Hearts", "A"));
+//     std::string output = captureOutput([]() { GUI::displayGameState(); });
+//     EXPECT_TRUE(output.find("Alice") != std::string::npos);
+//     EXPECT_TRUE(output.find("Bob") != std::string::npos);
+//     EXPECT_TRUE(output.find("A") != std::string::npos);
+// }
+
+// TEST_F(GUITest, DisplayPlayerMove) {
+//     Player* player = new Player("Alice", 1000);
+//     std::string output = captureOutput([&]() { GUI::displayPlayerMove(player, "raise", 50); });
+//     EXPECT_TRUE(output.find("Alice: raise 50") != std::string::npos);
+//     delete player;
+// }
+
+// TEST_F(GUITest, GetRandomPlayerName) {
+//     std::string name = GUI::getRandomPlayerName();
+//     EXPECT_FALSE(name.empty());
+// }
+
+// int main(int argc, char **argv) {
+//     testing::InitGoogleTest(&argc, argv);
 //     return RUN_ALL_TESTS();
 // }
